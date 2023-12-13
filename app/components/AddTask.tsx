4fe4812b3fc78bf49540c99e6aeb256bc5caf1f0
Sedjro2000 <manwenselove@gmail.com>
2023-12-13 13:18:34 +0100

    premier  commit

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,11 @@
+import { GoPlus } from "react-icons/go";
+const AddTask = () => {
+  return (
+    <div>
+        <button className="btn btn-primary w-full">Add new Task
+        <GoPlus className="ml-2" size ={20}/></button>
+    </div>
+  )
+}
+
+export default AddTask
